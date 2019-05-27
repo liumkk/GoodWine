4c9052df6643b38b679b96cf2104250922162210
liumengkai <lliumengkai@163.com>
2019-05-27 22:04:09 +0800

    新建项目

---
+++ GoodWine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoodWine
+//
+//  Created by LiuMengkai on 2019/5/27.
+//  Copyright © 2019年 LMK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
